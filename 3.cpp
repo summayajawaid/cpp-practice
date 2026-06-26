5f9eea1c6b2ef0cd887d81e11dccf1bbcf1cc2c2
Summaya Jawaid <summayaaa3@gmail.com>
2026-06-26 22:06:47 +0500

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+//print even numbers from 1 to 20
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1; i<=20 ;i++)
+    {
+        if(i%2==0)
+        {
+            cout<<i<<" "<<endl;
+        }
+    }
+}
