7eb14c7c89e020684f24e69f077b65300dde9b8e
Summaya Jawaid <summayaaa3@gmail.com>
2026-06-29 23:29:25 +0500

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+//Take input n and print 1 → n
+#include<iostraem>
+using namespace std;
+int maim()
+{
+    int n;
+    cout<<"Enter a number "<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+    cout<<i<<endl;
+    }
+}
