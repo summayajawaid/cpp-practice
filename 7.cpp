23a30a4de982516113d7361bc858b4af4a2b8b73
Summaya Jawaid <summayaaa3@gmail.com>
2026-07-02 20:31:13 +0500

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout <<"Enter a numbere" <<endl;
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<i<<"*"<<n<<"="<<i*n<<endl;
+    }
+}
