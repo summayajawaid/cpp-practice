1a56ffa8d22addf9f8b5165939b74f07022d6de7
Summaya Jawaid <summayaaa3@gmail.com>
2026-07-11 21:30:43 +0500

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,16 @@
+// Reverse a number (123 → 321)
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, reversed = 0, remainder;
+    cout << "Enter an integer: ";
+    cin >> n;
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+    cout << "Reversed number: " << reversed << endl;
+    return 0;
+}
