fb2911cc0a9a73bd023e5919d9e4988a2bb7f583
Summaya Jawaid <summayaaa3@gmail.com>
2026-07-16 20:48:52 +0500

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,25 @@
+//Check if a number is palindrome
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int n) {
+    int original = n;
+    int reversed = 0;
+    while (n > 0) {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+    return original == reversed;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (isPalindrome(num)) {
+        cout << num << " is a palindrome." << endl;
+    } else {
+        cout << num << " is not a palindrome." << endl;
+    }
+    return 0;
+}
