de99864ada691c6fb93c1bc00f14c2ffd0a59f2c
Summaya Jawaid <summayaaa3@gmail.com>
2026-07-19 22:53:13 +0500

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,16 @@
+//Find sum of array.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    int sum = 0;
+    cout << "Enter 5 elements: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << "Sum of array elements: " << sum << endl;
+    return 0;
+}
