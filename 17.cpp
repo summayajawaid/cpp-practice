9b966305ca763cb1319ddbffd7d0331c1385fead
Summaya Jawaid <summayaaa3@gmail.com>
2026-07-27 22:20:12 +0500

    Add files via upload

---
+++ 17.cpp
@@ -0,0 +1,19 @@
+//Count odd numbers in array
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int oddCount = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 != 0) {
+            oddCount++;
+        }
+    }
+
+    cout << "Number of odd numbers in the array: " << oddCount << endl;
+
+    return 0;
+}
