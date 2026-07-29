e444a3cbbb42c25a6b49b64779d8eabb1944b6dc
Summaya Jawaid <summayaaa3@gmail.com>
2026-07-29 21:37:14 +0500

    Add files via upload

---
+++ 18.cpp
@@ -0,0 +1,27 @@
+//Reverse array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // Reverse the array
+    for (int i = 0; i < size / 2; i++) {
+        swap(arr[i], arr[size - 1 - i]);
+    }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
